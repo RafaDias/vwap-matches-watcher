3afaf57bb99cc3226c66e90785b6004551afcbbf
Rafael Dias <rafaeldiasmello@gmail.com>
2022-03-13 13:04:30 -0300

    Create add receiver to allow store datapoints

--- internal/domain/domain.go
+++ internal/domain/domain.go
@@ -23,3 +23,22 @@ type TradingPair struct {
 func (tp *TradingPair) VWAP() float64 {
 	return tp.Volume / tp.Size
 }
+
+func (tp *TradingPair) Add(p Price) error {
+	if p.Price <= 0 || p.Size <= 0 {
+		return errors.New("you must provide positive number")
+	}
+
+	if tp.isFull() {
+		tp.dropOldest()
+	}
+	tp.Prices = append(tp.Prices, p)
+	tp.Size += p.Size
+	tp.Volume += p.Size * p.Price
+
+	return nil
+}
+
+func (tp *TradingPair) isFull() bool {
+	return len(tp.Prices) == tp.WindowSize
+}
